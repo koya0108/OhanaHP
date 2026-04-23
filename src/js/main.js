404c9666e68d425e983caab8c1bfc7ef47d1f40d
koya <koyamaguchi.4@gmail.com>
2026-04-23 22:49:56 +0900

    PC版作成

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-console.log("Ohana HP");
+const faqQuestions = document.querySelectorAll(".faq-question");
+
+faqQuestions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const faqItem = question.parentElement;
+    faqItem.classList.toggle("active");
+  });
+});
+
+const elements = document.querySelectorAll(".fade-up");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  });
+
+  elements.forEach((el) => observer.observe(el));
+
+  
